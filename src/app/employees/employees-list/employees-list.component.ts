c01bd20de3489cd072236094c23e931c703d39a6
kaushik867 <kaushikk867@gmail.com>
2021-01-29 17:33:41 +0530

    correction sorting

--- src/app/employees/employees-list/employees-list.component.ts
+++ src/app/employees/employees-list/employees-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { LoaderService } from 'src/app/loader/service/loader.service';
 import { EmployeesListServiceService } from '../services/employees-list-service.service';
@@ -15,31 +15,30 @@ import { User } from 'src/app/modal/user.modal';
 })
 
 
-export class EmployeesListComponent implements OnInit, OnDestroy {
+export class EmployeesListComponent implements OnInit, OnDestroy, AfterViewInit {
   displayedColumns: string[] = ['id', 'firstname', 'lastname', 'company', 'job'];
-  dataSource:MatTableDataSource<User>;
-  @ViewChild(MatSort) sort: MatSort;
-  @ViewChild(MatPaginator)paginator: MatPaginator;
+  dataSource = new MatTableDataSource<User>();
+  @ViewChild(MatSort,{static:true}) sort: MatSort;
+  @ViewChild(MatPaginator,{static:true})paginator: MatPaginator;
   subscription: any;
 
-
-  constructor(public loader: LoaderService, private _http: EmployeesListServiceService, private route: Router) {
-     this.subscription = this._http.getEmployess().subscribe(data=>{      
-      this.dataSource = new MatTableDataSource(data);
-      this.dataSource.sort = this.sort;
-      this.dataSource.paginator = this.paginator;
+  constructor(public loader: LoaderService, private _http: EmployeesListServiceService, private route: Router) {}
+  
+  ngOnInit(): void {
+    this.subscription = this._http.getEmployess().subscribe(data=>{      
+      this.dataSource.data = [...data];
     });
-    
   }
+
+  ngAfterViewInit(): void {
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
+  }
+
   ngOnDestroy(): void {
     this.subscription.unsubscribe();
   }
   
-   
-  ngOnInit(): void {
-   
-  }
-
   getId(user) {
     this.route.navigate(['/employees/details',user.id]);
   }
