10a74e581899db736d9ad5080b23dc5e8e71bd29
kaushik867 <kaushikk867@gmail.com>
2021-01-29 16:12:43 +0530

    Sovling sorting issue

--- src/app/employees/employees-list/employees-list.component.ts
+++ src/app/employees/employees-list/employees-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { Component,AfterViewInit, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { LoaderService } from 'src/app/loader/service/loader.service';
 import { EmployeesListServiceService } from '../services/employees-list-service.service';
@@ -18,33 +18,34 @@ import * as fromEmp from '../state/emp.selector';
 })
 
 
-export class EmployeesListComponent implements OnInit, OnDestroy {
+export class EmployeesListComponent implements OnInit, OnDestroy, AfterViewInit {
   displayedColumns: string[] = ['id', 'firstname', 'lastname', 'company', 'job','delete'];
   dataSource$: Observable<User[]>;
-  dataSource: MatTableDataSource<User>;
-  @ViewChild(MatSort) sort: MatSort;
-  @ViewChild(MatPaginator)paginator: MatPaginator;
+  dataSource= new MatTableDataSource<User>();
+  @ViewChild(MatSort,{static:true}) sort: MatSort;
+  @ViewChild(MatPaginator,{static: true})paginator: MatPaginator;
   subscription: any;
 
 
-  constructor(private store: Store<any>,public loader: LoaderService, private _http: EmployeesListServiceService, private route: Router) { 
+  constructor(private store: Store<any>,public loader: LoaderService, private _http: EmployeesListServiceService, private route: Router) { }
+  
+   
+  ngOnInit(): void {
     this.store.dispatch(EmpAction.loadEmployee());
     this.dataSource$ = this.store.select(fromEmp.getEmp);
     this.subscription = this.dataSource$.subscribe(data=>{
-      this.dataSource= new MatTableDataSource(data);
-      this.dataSource.sort = this.sort;
-      this.dataSource.paginator = this.paginator;
-    })
-    console.log(this.dataSource );
-   }
-  
+      this.dataSource.data = [...data];
+    });
+  }
+
+
   ngOnDestroy(): void {
     this.subscription.unsubscribe();
   }
   
-   
-  ngOnInit(): void {
-    
+  ngAfterViewInit():void{
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
   }
 
   getId(user) {
@@ -56,8 +57,7 @@ export class EmployeesListComponent implements OnInit, OnDestroy {
     {
       console.log(user.id);
       this.store.dispatch(EmpAction.deleteEmployee({id:user.id}));
-    }
-
+    } 
   }
   
   applySearch(fieldvalue){
